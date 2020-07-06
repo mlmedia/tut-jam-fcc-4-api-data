729fde7b8cbd6db81952cfe70251d10ccd8ea4b8
mlmedia <mike@millionlightsmedia.com>
2020-07-06 19:07:41 -0400

    - initial commit - moving code to exercise 4 repo

---
+++ src/_data/news.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+require('dotenv').config();
+
+module.exports = async function () {
+	try {
+		const response = await axios.get(`http://newsapi.org/v2/everything?domains=wsj.com&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
